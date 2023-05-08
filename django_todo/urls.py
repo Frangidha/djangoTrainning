0c7bd8f9165135ecdbd8a8121468b5b67b5b7598
Frangidha <>
2023-05-08 06:45:04 +0000

    first deployment

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import views
+from todo import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.get_todo_list, name='get_todo_list'),
-    path('add', views.add_item, name='add_item'),
+    path('add', views.add_item, name='add'),
     path('edit/<item_id>', views.edit_item, name='edit'),
     path('toggle/<item_id>', views.toggle_item, name='toggle'),
-    path('toggle/<item_id>', views.delete_item, name='delete'),
+    path('delete/<item_id>', views.delete_item, name='delete'),
+
 ]
